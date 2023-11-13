7bb6683c065d872c04b888d97b6d3242ca2c59ee
a23pablooc <a23pablooc@iessanclemente.net>
2023-11-13 23:27:09 +0100

    Modelo de CardView para medicamentos favoritos

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/FavoriteFragment.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/FavoriteFragment.kt
@@ -3,15 +3,37 @@ package com.example.uf1_proyecto
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.uf1_proyecto.databinding.FragmentFavoriteBinding
+import com.google.android.material.navigation.NavigationView
 
 class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val toolbar = binding.favoriteFragmentToolbar
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val navHostFragment = (activity as AppCompatActivity).supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
         return view
     }
 
