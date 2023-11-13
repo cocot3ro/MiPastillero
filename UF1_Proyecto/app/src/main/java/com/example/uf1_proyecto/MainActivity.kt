f9070aedbb04f50d439d60cd17dab4d1d2527a53
a23pablooc <a23pablooc@iessanclemente.net>
2023-11-13 12:45:48 +0100

    Creada bottom bar

--- UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
+++ UF1_Proyecto/app/src/main/java/com/example/uf1_proyecto/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.uf1_proyecto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Controlador de navegación - Host de navegación (grafo asociado)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //Panel Lateral - Referencia Drawer Layout
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawerLayout)
+
+        //Configuración de la barra de navegación
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, findNavController(R.id.nav_host_fragment))
     }
 }
