518cc4025cbffafd761d6c8c4b60ce12376084c6
a23pablooc <a23pablooc@iessanclemente.net>
2024-06-04 17:00:15 +0200

    Clases iniciales para RecyclerView de CalendarMedGroup

---
+++ ProxectoFCT/app/src/main/java/com/a23pablooc/proxectofct/ui/view/viewholders/CalendarMedGroupViewHolder.kt
@@ -0,0 +1,13 @@
+package com.a23pablooc.proxectofct.ui.view.viewholders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.a23pablooc.proxectofct.databinding.CalendarMedGroupBinding
+
+class CalendarMedGroupViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val binding = CalendarMedGroupBinding.bind(view)
+
+    fun render() {
+        
+    }
+}
