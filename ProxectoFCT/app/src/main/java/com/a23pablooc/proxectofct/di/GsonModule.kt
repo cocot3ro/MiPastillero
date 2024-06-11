bfa3762a5f246262caeb877b4495981d58dd5eff
a23pablooc <a23pablooc@iessanclemente.net>
2024-06-11 13:37:32 +0200

    Modulo para inyectar Gson

---
+++ ProxectoFCT/app/src/main/java/com/a23pablooc/proxectofct/di/GsonModule.kt
@@ -0,0 +1,28 @@
+package com.a23pablooc.proxectofct.di
+
+import com.a23pablooc.proxectofct.data.model.typeadapters.MedicamentoModelTypeAdapter
+import com.a23pablooc.proxectofct.data.model.typeadapters.UriTypeAdapter
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GsonModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(): Gson {
+        return GsonBuilder()
+            .registerTypeAdapter(
+                MedicamentoModelTypeAdapter::class.java,
+                MedicamentoModelTypeAdapter()
+            )
+            .registerTypeAdapter(UriTypeAdapter::class.java, UriTypeAdapter())
+            .create()
+    }
+}
